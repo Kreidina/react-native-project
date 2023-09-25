d7e287fd2b6831ed17835ba7cea565ac048337e1
Kreidina <kav99mail@gmail.com>
2023-09-25 23:05:16 +0300

    config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKER,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiOUnkKXevLGzWTBemMWQCUSI8NaaVsLs",
-  authDomain: "react-native-project-7e7fa.firebaseapp.com",
-  projectId: "react-native-project-7e7fa",
-  storageBucket: "react-native-project-7e7fa.appspot.com",
-  messagingSenderId: "893441243562",
-  appId: "1:893441243562:web:ce4b93c1538f6087cb13d3",
-  measurementId: "G-P0Z6T4H3EC",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKER,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
